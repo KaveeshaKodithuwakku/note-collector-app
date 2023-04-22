8c28a317ceac02d1e97a8f30bd7d44d0efe916ce
KaveeshaKodithuwakku <k.kaveesha95@gmail.com>
2023-04-22 13:12:14 +0530

    Bug fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import NavBar from './components/NavBar/NavBar';
 import Login from './pages/login/Login'
 import Home from './pages/home/Home'
-import AddNotes from './pages/addNotes/AddNotes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ViewNotes from './pages/viewNotes/ViewNotes';
 import SignUp from './pages/signUp/SignUp';
@@ -22,10 +20,8 @@ function App() {
     <div>
 
 <AuthContextProvider>
-{/* <NavBar/> */}
       <Routes>
         <Route  path='/home' element={<Home/>}/>
-        {/* <Route  path='/add' element={<AddNotes/>}/>  */}
         <Route  path='/view' element={<ViewNotes/>}/> 
         <Route  path='/calendar' element={<Calendar/>}/> 
         <Route  path='/favorite-notes' element={<FavoriteNotes/>}/> 
